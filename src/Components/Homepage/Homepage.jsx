c9231991940e1c83990bc819f1fe09cbac5b4dd8
SajeedAninda <sajaninda@gmail.com>
2023-11-04 21:29:55 +0600

    Created The footer Section

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Newsletter from '../NewsLetter/Newsletter';
+import Footer from '../Footer/Footer';
 
 const Homepage = () => {
     return (
-       <>
+        <>
             <Banner></Banner>
             <Newsletter></Newsletter>
-       </>
+            <Footer></Footer>
+        </>
     );
 };
 
