135f923e1415fda5ebb85c4c1241389fd87cf887
SajeedAninda <sajaninda@gmail.com>
2023-11-04 20:43:32 +0600

    Added Font-Lora

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ import bannerLottie from "../../assets/Banner/bannerLottie.json"
 const Banner = () => {
     return (
         <div className='bg-[#fcf4e9]'>
-            <div className='w-[90%] h-screen mx-auto flex items-center justify-between'>
+            <div className='w-[90%] h-fit py-16 mx-auto flex items-center justify-between'>
                 <div className='flex-1 space-y-2'>
                     <h2 className='text-[#1b1f20] font-bold text-4xl'>Delightful Blogs</h2>
                     <h1 className='text-[#1b1f20] font-bold text-6xl'>Connecting Hearts and Minds Through the Art of Blogging</h1>
