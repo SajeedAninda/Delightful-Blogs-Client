2752e6156a7f932b90bd84511f0f83c0132547dc
SajeedAninda <sajaninda@gmail.com>
2023-11-06 23:28:33 +0600

    Added Animation to banner header using Framer Motion

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import Lottie from "lottie-react";
 import bannerLottie from "../../assets/Banner/bannerLottie.json"
+import { motion } from 'framer-motion';
 
 const Banner = () => {
+    const animationVariants = {
+        initial: { opacity: 0, x: -100 },
+        animate: { opacity: 1, x: 0 },
+      };
     return (
         <div className='bg-[#fcf4e9]'>
             <div className='w-[90%] h-fit py-16 mx-auto flex flex-col md:flex-row items-center justify-between'>
-                <div className='flex-1 space-y-2'>
+                <motion.div
+                    variants={animationVariants}
+                    initial="initial"
+                    animate="animate"
+                    transition={{ duration: 1 }}
+                    className='flex-1 space-y-2'>
                     <h2 className='text-[#1b1f20] font-bold text-2xl md:text-3xl lg:text-4xl'>Delightful Blogs</h2>
                     <h1 className='text-[#1b1f20] font-bold text-4xl md:text-5xl lg:text-6xl'>Connecting Hearts and Minds Through the Art of Blogging</h1>
-                </div>
+                </motion.div>
 
                 <div className='flex-1'>
                     <Lottie animationData={bannerLottie} loop={true} />
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
